ceccf5e5216cecf988187046cf3bf9c52cc21e68
erinkelsey <erinszilagyi@gmail.com>
2020-08-08 13:44:28 -0500

    Cleanup skills view.

--- lib/views/skills/skills_desktop_view.dart
+++ lib/views/skills/skills_desktop_view.dart
@@ -12,12 +12,10 @@ class SkillsDesktopView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
 
     return Container(
       width: width * 0.8,
-      // height: height * 0.45,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
